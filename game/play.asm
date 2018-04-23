9a1e52f22aa410a080d2c34e5cc271334411d24b
TomerCnaan <cnaan.tomer@gmail.com>
2018-04-23 19:35:48 +0300

    ss

--- game/play.asm
+++ game/play.asm
@@ -6,3 +6,49 @@
 ; Description: 
 ;===================================================================================================
 LOCALS @@
+
+
+
+
+;------------------------------------------------------------------------
+;  : 
+; 
+; Input:
+;     push  X1 
+;     push  X2
+;     call  
+; 
+; Output: 
+;     AX - 
+; 
+; Affected Registers: 
+; Limitations: 
+;------------------------------------------------------------------------
+PROC  
+    push bp
+    mov bp,sp
+    ;sub sp,2            ;<- set value
+    pusha
+ 
+    ; now the stack is
+    ; bp-2 => 
+    ; bp+0 => old base pointer
+    ; bp+2 => return address
+    ; bp+4 => 
+    ; bp+6 => 
+    ; saved registers
+ 
+    ;{
+    varName_         equ        [word bp-2]
+ 
+    parName2_        equ        [word bp+4]
+    parName1_        equ        [word bp+6]
+    ;}
+ 
+@@end:
+    popa
+    mov sp,bp
+    pop bp
+    ret ;4               ;<- set value
+ENDP  
+;ss
