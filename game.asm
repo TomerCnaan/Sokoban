262f517af9600be4f2d7bf59103c50dc8aa73cad
unknown <oded.cnaan@gmail.com>
2018-04-23 19:01:37 +0300

    prog

--- game.asm
+++ game.asm
@@ -1,1 +1,30 @@
-asdfjkhgkjsadf
+;===================================================================================================
+; Written By: Tomer Cnaan (212789382)
+; Date: 23-04-2018
+; File: game.asm
+;
+; Description: 
+;===================================================================================================
+LOCALS @@
+
+.486
+IDEAL
+MODEL small
+STACK 100h
+
+DATASEG
+
+
+CODESEG
+start:
+    mov ax, @data
+    mov ds,ax
+
+    ; Code goes here
+
+exit:
+    mov ah, 4ch
+    mov al, 0
+    int 21h
+END start
+CODSEG ends
